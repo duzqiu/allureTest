1a63f7a209c14b6e0d3969485820140e149e95f7
duzqiu <duzqiu@outlook.com>
2020-07-07 15:28:01 +0800

    Initial commit

---
+++ scripts/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Time    : 2020/7/7 15:21
+# @Author  : DZQ
+# @File    : __init__.py.py
+# @Software: PyCharm
---
+++ scripts/testLogin.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Time    : 2020/7/7 15:22
+# @Author  : DZQ
+# @File    : testLogin.py
+# @Software: PyCharm
+
+import pytest
+
+
+class TestLogin(object):
+    def setup_class(self):
+        pass
+
+    def teardown_class(self):
+        pass
+
+    def test001(self):
+        print("这是TEST001")
+
+    def test002(self):
+        print("这是TEST002")
